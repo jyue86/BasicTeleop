9b7bc478d27e637a9fd209c5b997d50569d5a2e7
Justin Yue <jyue009@ucr.edu>
2025-12-17 20:44:22 -0800

    realsense stream to visualizer

---
+++ cpp/src/realsense.cpp
@@ -0,0 +1,188 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "realsense.hpp"
+
+#include "cuda_runtime.h"
+#include "gxf/multimedia/camera.hpp"
+#include "gxf/multimedia/video.hpp"
+#include "librealsense2/rs.hpp"
+#include <holoscan/operators/holoviz/holoviz.hpp>
+
+namespace holoscan::ops {
+
+void RealsenseCameraOp::setup(OperatorSpec& spec) {
+  spec.output<holoscan::gxf::Entity>("color_buffer");
+  spec.output<holoscan::gxf::Entity>("depth_buffer");
+
+  spec.output<nvidia::gxf::CameraModel>("color_camera_model").condition(ConditionType::kNone);
+  spec.output<nvidia::gxf::CameraModel>("depth_camera_model").condition(ConditionType::kNone);
+
+  spec.param(allocator_, "allocator", "Allocator", "Allocator to allocate output tensor.");
+}
+
+void RealsenseCameraOp::start() {
+  rs2::config config;
+  try {
+    config.enable_stream(RS2_STREAM_COLOR, 1280, 800, RS2_FORMAT_RGB8, 30);
+    config.enable_stream(RS2_STREAM_DEPTH, 1280, 720, RS2_FORMAT_Z16, 30);
+    profile_ = pipeline_.start(config);
+    HOLOSCAN_LOG_INFO("Realsense camera started.");
+  } catch (const rs2::error& e) {
+    HOLOSCAN_LOG_WARN("RealSense error calling {}: {}", e.get_failed_function(), e.what());
+
+    // Do not hardcode resolution/FPS to allow auto-negotiation (e.g. for USB2 or different camera models)
+    config.enable_stream(RS2_STREAM_COLOR, RS2_FORMAT_RGB8);
+    config.enable_stream(RS2_STREAM_DEPTH, RS2_FORMAT_Z16);
+    profile_ = pipeline_.start(config);
+    
+    auto color_stream = profile_.get_stream(RS2_STREAM_COLOR).as<rs2::video_stream_profile>();
+    auto depth_stream = profile_.get_stream(RS2_STREAM_DEPTH).as<rs2::video_stream_profile>();
+    HOLOSCAN_LOG_INFO("Realsense camera started with fallback. Color: {}x{} @ {}fps, Depth: {}x{} @ {}fps", 
+        color_stream.width(), color_stream.height(), color_stream.fps(),
+        depth_stream.width(), depth_stream.height(), depth_stream.fps());
+  }
+}
+
+void RealsenseCameraOp::stop() {
+  pipeline_.stop();
+  HOLOSCAN_LOG_INFO("Realsense camera stopped.");
+}
+
+void RealsenseCameraOp::compute(InputContext& op_input, OutputContext& op_output,
+                                ExecutionContext& context) {
+  HOLOSCAN_LOG_INFO("Starting compute step.");
+  auto allocator = nvidia::gxf::Handle<nvidia::gxf::Allocator>::Create(
+      fragment()->executor().context(), allocator_->gxf_cid());
+
+  // Wait for the next set of camera frames.
+  rs2::frameset frameset =
+      pipeline_.wait_for_frames().apply_filter(align_).apply_filter(units_transform_);
+  rs2::video_frame color_frame = frameset.get_color_frame();
+  rs2::depth_frame depth_frame = frameset.get_depth_frame();
+
+  // Emit the color buffer.
+  auto color_buffer_message = gxf::Entity::New(&context);
+  nvidia::gxf::Handle<nvidia::gxf::VideoBuffer> color_buffer =
+      static_cast<nvidia::gxf::Entity>(color_buffer_message)
+          .add<nvidia::gxf::VideoBuffer>("color_buffer")
+          .value();
+  color_buffer->resize<nvidia::gxf::VideoFormat::GXF_VIDEO_FORMAT_RGB>(
+      color_frame.get_width(),
+      color_frame.get_height(),
+      nvidia::gxf::SurfaceLayout::GXF_SURFACE_LAYOUT_PITCH_LINEAR,
+      nvidia::gxf::MemoryStorageType::kDevice,
+      allocator.value());
+  nvidia::gxf::VideoBufferInfo color_buffer_info = color_buffer->video_frame_info();
+  assert(color_buffer_info.color_planes[0].stride == color_frame.get_stride_in_bytes());
+  assert(color_buffer_info.color_planes[0].bytes_per_pixel == color_frame.get_bytes_per_pixel());
+  cudaError_t cuda_error = cudaMemcpy(color_buffer->pointer(),
+                                      color_frame.get_data(),
+                                      color_frame.get_data_size(),
+                                      cudaMemcpyHostToDevice);
+  if (cuda_error != cudaSuccess) {
+    throw std::runtime_error("cudaMemcpy() failed for color_frame");
+  }
+  HOLOSCAN_LOG_INFO("Color frame copied to device buffer, time to emit.");
+  op_output.emit(color_buffer_message, "color_buffer");
+
+  // Emit the color camera model.
+  // TODO: Add distortion models.
+  rs2_intrinsics color_intrinsics =
+      color_frame.get_profile().as<rs2::video_stream_profile>().get_intrinsics();
+  const nvidia::gxf::CameraModel color_camera_model{
+      .dimensions = {static_cast<uint32_t>(color_intrinsics.width),
+                     static_cast<uint32_t>(color_intrinsics.height)},
+      .focal_length = {color_intrinsics.fx, color_intrinsics.fy},
+      .principal_point = {color_intrinsics.ppx, color_intrinsics.ppy},
+      .distortion_type = nvidia::gxf::DistortionType::Perspective,
+  };
+  op_output.emit(color_camera_model, "color_camera_model");
+
+  // Emit the depth buffer.
+  auto depth_buffer_message = gxf::Entity::New(&context);
+  nvidia::gxf::Handle<nvidia::gxf::VideoBuffer> depth_buffer =
+      static_cast<nvidia::gxf::Entity>(depth_buffer_message)
+          .add<nvidia::gxf::VideoBuffer>("depth_buffer")
+          .value();
+  depth_buffer->resize<nvidia::gxf::VideoFormat::GXF_VIDEO_FORMAT_GRAY16>(
+      depth_frame.get_width(),
+      depth_frame.get_height(),
+      nvidia::gxf::SurfaceLayout::GXF_SURFACE_LAYOUT_PITCH_LINEAR,
+      nvidia::gxf::MemoryStorageType::kDevice,
+      allocator.value());
+  nvidia::gxf::VideoBufferInfo depth_buffer_info = depth_buffer->video_frame_info();
+  assert(depth_buffer_info.color_planes[0].stride == depth_frame.get_stride_in_bytes());
+  assert(depth_buffer_info.color_planes[0].bytes_per_pixel == depth_frame.get_bytes_per_pixel());
+  cuda_error = cudaMemcpy(depth_buffer->pointer(),
+                          depth_frame.get_data(),
+                          depth_frame.get_data_size(),
+                          cudaMemcpyHostToDevice);
+  if (cuda_error != cudaSuccess) {
+    throw std::runtime_error("cudaMemcpy() failed for depth_frame");
+  }
+  HOLOSCAN_LOG_INFO("Depth frame copied to device buffer, time to emit.");
+  op_output.emit(depth_buffer_message, "depth_buffer");
+
+  // Emit the depth camera model.
+  // TODO: Add distortion models.
+  rs2_intrinsics depth_intrinsics =
+      depth_frame.get_profile().as<rs2::video_stream_profile>().get_intrinsics();
+  const nvidia::gxf::CameraModel depth_camera_model{
+      .dimensions = {static_cast<uint32_t>(depth_intrinsics.width),
+                     static_cast<uint32_t>(depth_intrinsics.height)},
+      .focal_length = {depth_intrinsics.fx, depth_intrinsics.fy},
+      .principal_point = {depth_intrinsics.ppx, depth_intrinsics.ppy},
+      .distortion_type = nvidia::gxf::DistortionType::Perspective,
+  };
+  op_output.emit(depth_camera_model, "depth_camera_model");
+}
+
+}  // namespace holoscan::ops
+
+using namespace holoscan;
+
+void RealsenseApp::compose() {
+    auto allocator = make_resource<UnboundedAllocator>("allocator");
+    auto realsense_camera_op = make_operator<ops::RealsenseCameraOp>(
+        "realsense_camera_op",
+        make_condition<PeriodicCondition>("frame_limiter", Arg("recess_period", std::string("60Hz"))),
+        Arg("allocator", allocator));
+    std::vector<ops::HolovizOp::InputSpec> input_spec;
+    auto& color_spec = input_spec.emplace_back(
+        ops::HolovizOp::InputSpec("color_buffer", ops::HolovizOp::InputType::COLOR));
+    color_spec.views_.push_back(ops::HolovizOp::InputSpec::View{0.0f, 0.0f, 0.5f, 1.0f});
+    auto& depth_spec = input_spec.emplace_back(
+        ops::HolovizOp::InputSpec("depth_buffer", ops::HolovizOp::InputType::COLOR));
+    depth_spec.views_.push_back(ops::HolovizOp::InputSpec::View{0.5f, 0.0f, 0.5f, 1.0f});
+
+    auto visualizer = make_operator<ops::HolovizOp>(
+        "holoviz",
+        Arg("window_title", "Realsense Visualization"),
+        Arg("width", 1280U * 2),
+        Arg("height", 720U * 2),
+        Arg("tensors", input_spec));
+
+    add_flow(
+        realsense_camera_op, visualizer, {{"color_buffer", "receivers"}, {"depth_buffer", "receivers"}});
+}
+
+int main() {
+    auto app = holoscan::make_application<RealsenseApp>();
+    app->run();
+    return 0;
+}
